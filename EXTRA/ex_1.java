4aa6c56137b4925cb6193e09983fd1e594928e77
Omar Al-mashaqbeh <o.sh.almashaqba@gmail.com>
2026-03-10 15:59:42 +0300

    Complete the solution of the second page

---
+++ EXTRA/ex_1.java
@@ -0,0 +1,9 @@
+package LapSheet_Solution.EXTRA;
+
+public class ex_1 {
+    public static void main(String[] args) {
+        double length = 12;
+        double width = 2;
+        System.out.println("Area = "+(length * width));
+    }
+}
---
+++ LapSheet_Solution/PAGE_02/Question_3.java
@@ -0,0 +1,13 @@
+package LapSheet_Solution.PAGE_02;
+
+public class Question_3 {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 1;
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int result = (3 + 4*x)/5 - (10*(y-5)*(a+b+c))/x + 9*((4/x) + (9+x)/y);
+        System.out.println("result = "+result);
+    }
+}
