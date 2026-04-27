586fc0739299c4f03a268c4e07f68b0baaaf3254
Omar Al-mashaqbeh <o.sh.almashaqba@gmail.com>
2026-04-27 19:21:34 +0300

    FIX

--- LapSheet_Solution/PAGE_02/Question_1.java
+++
@@ -1,7 +0,0 @@
-package LapSheet_Solution.PAGE_02;
-
-public class Question_1 {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Java class");
-    }
-}
--- LapSheet_Solution/PAGE_03/Question_2_Dialog.java
+++
@@ -1,9 +0,0 @@
-package LapSheet_Solution.PAGE_03;
-import javax.swing.JOptionPane;
-public class Question_2_Dialog {
-    public static void main(String[] args) {
-        String str = JOptionPane.showInputDialog(null, "Enter the character");
-        char ch = str.charAt(0);
-        JOptionPane.showMessageDialog(null, "The Ascii code of \'"+(ch)+"\' is "+(int)ch);
-    }
-}
